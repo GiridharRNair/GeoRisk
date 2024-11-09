3d4d7007e42aefe6a1020071ef113ef2f9fe1ce5
Giridhar Nair <Giridhar.r.nair@gmail.com>
2024-11-09 11:49:34 -0600

    cleanup

---
+++ README.md
@@ -0,0 +1,7 @@
+front end
+React.js
+https://visgl.github.io/react-map-gl/examples/geocoder
+
+Backend
+
+python Fast api
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+const mapBoxAPIKey = "pk.eyJ1Ijoidmlld3Nmcm9tdGhlNml4IiwiYSI6ImNtM2FnNHBlMjFnMjIybXB6dXV5eGpvOG4ifQ.pHD9qGKe5uE26mYuHM4r2g";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Vite + React</h1>
+            <div>
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p>Click on the Vite and React logos to learn more</p>
+        </>
+    );
 }
 
-export default App
+export default App;
